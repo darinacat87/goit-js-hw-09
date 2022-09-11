1f1e66821d57fffc000ab0532544847c9430fd7d
Daria Sliusar <darinacat87@gmail.com>
2022-09-11 16:59:45 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  bodyEl: document.querySelector('body'),
+  btnStartEl: document.querySelector('[data-start]'),
+  btnStopEl: document.querySelector('[data-stop]'),
+};
+
+let timerId = null;
+refs.btnStopEl.disabled = true;
+
+refs.btnStartEl.addEventListener('click', () => {
+  refs.btnStartEl.disabled = true;
+  refs.btnStopEl.disabled = false;
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    refs.bodyEl.style.background = color;
+  }, 1000);
+});
+
+refs.btnStopEl.addEventListener('click', () => {
+  refs.btnStartEl.disabled = false;
+  refs.btnStopEl.disabled = true;
+  clearTimeout(timerId);
+});
